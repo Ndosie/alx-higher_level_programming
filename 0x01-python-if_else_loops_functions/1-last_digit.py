157c09ede48dc446f19024972a3155612378c8e8
Esther Ndosi <estherndosi24@gmail.com>
2024-03-12 03:21:14 -0700

    Check last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10
 if last_digit > 0:
-    print("is positive")
+    print("{} is positive".format(last_digit))
 elif last_digit == 0:
-    print("is zero")
+    print("{} is zero".format(last_digit))
 else:
-    print("is negative")
+    print("{} is negative".format(last_digit))
