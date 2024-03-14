66b11a6c3fe322c8e9f94b1303aaff5321e19822
Esther Ndosi <estherndosi24@gmail.com>
2024-03-14 03:54:04 -0700

    Print module names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for a in dir(hidden_4):
-    if a[0] == "_":
-        continue
+if __name__ == "__main__":
+    for a in dir(hidden_4):
+        if a[0] == "_":
+            continue
 
     print(a)
