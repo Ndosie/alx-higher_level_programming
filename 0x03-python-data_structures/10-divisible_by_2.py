151edfd1a12e9543497619513fa0f22252e77cb1
Esther Ndosi <estherndosi24@gmail.com>
2024-03-19 01:28:21 -0700

    Print 2 divisibles

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    copy = my_list.copy()
     if len(my_list) == 0:
         return my_list
 
-    for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
-            my_list[i] = True
+    for i in range(len(copy)):
+        if copy[i] % 2 == 0:
+            copy[i] = True
         else:
-            my_list[i] = False
-    return my_list
+            copy[i] = False
+    return copy
