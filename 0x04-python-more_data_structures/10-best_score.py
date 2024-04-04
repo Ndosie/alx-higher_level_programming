480fca098823a04d8f3c407e16f1f69a47e39395
Esther Ndosi <estherndosi24@gmail.com>
2024-04-04 05:35:30 -0700

    Find maximum item in the dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if(a_dictionary == None):
+        return None
+
+    m = max(a_dictionary.values())
+    for k, v in a_dictionary.items():
+        if a_dictionary[k] == m:
+            return k
