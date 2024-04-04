c4a2c36917443062a6d9bf9c8ef8747a9540ffdd
Esther Ndosi <estherndosi24@gmail.com>
2024-04-04 04:11:01 -0700

    Find sum of unique integers from a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -5,7 +5,3 @@ def uniq_add(my_list=[]):
     for i in s:
         results += i
     return results
-
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
