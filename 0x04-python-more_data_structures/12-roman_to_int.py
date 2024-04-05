84fc8d22666340012f8c05e256f7555a9910872f
Esther Ndosi <estherndosi24@gmail.com>
2024-04-05 01:00:01 -0700

    Roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     result = 0
     i = 0
 
-    if roman_string is None or roman_string.isdigit():
+    if roman_string is None or !isinstance(roman_string, str):
         return 0
 
     while i < len(roman_string):
