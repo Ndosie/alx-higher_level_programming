2052f124917c14bf413a34ab38ba3d5a3a7c4882
Esther Ndosi <estherndosi24@gmail.com>
2024-04-05 00:50:17 -0700

    Roman to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    i = 0
+
+    if roman_string is None or len(roman_string) == 0:
+        return 0
+
+    while i < len(roman_string):
+        if i + 1 < len(roman_string):
+            if romans[roman_string[i]] < romans[roman_string[i+1]]:
+                result += romans[roman_string[i + 1]] - romans[roman_string[i]]
+                i += 2
+                continue
+
+        result += romans[roman_string[i]]
+        i += 1
+
+    return result
