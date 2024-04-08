901b8c31f44519ed236132bdb31347875645dd79
Esther Ndosi <estherndosi24@gmail.com>
2024-04-08 06:08:47 -0700

    Defining a class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle:
         return self.__width
 
     def width(self, value):
-        if type(value) != 'int':
+        if not isinstance(value, 'int'):
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
@@ -24,7 +24,7 @@ class Rectangle:
         return self.__height
 
     def height(self, value):
-        if type(value) != 'int':
+        if not isinstance(value, 'int'):
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
