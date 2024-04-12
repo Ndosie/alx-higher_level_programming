591856d387ac9d91a1db73ee0385c8eb77807e29
Esther Ndosi <estherndosi24@gmail.com>
2024-04-12 04:58:10 -0700

    TDD print name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This is "3-say_my_name" module
+
+The 3-say_my_name module supplies one function say_my_name(). For example,
+
+>>> say_my_name("Esther", "Ndosi")
+My name is Esther Ndosi
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints the message with first_name and last_name"""
+
+    if type(first_name) != str:
+        raise TypeError('first_name must be a string')
+    if type(last_name) != str:
+        raise TypeError('last_name must be a string')
+
+    print("My name is {} {}".format(first_name, last_name))
