cafeb570073d11453e6e539a73da6367b7d0256f
Esther Ndosi <estherndosi24@gmail.com>
2024-04-12 06:01:56 -0700

    TDD print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This 4-print_square module
+
+The 4-print_square module has one function called ``print_square()``
+which prints `#` in a square shape according to a given size.
+For example
+
+>>> print_square(4)
+####
+####
+####
+####
+"""
+
+
+def print_square(size):
+    """Print # in square shape according to size"""
+
+    if type(size) != int:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    if size == 0:
+        print("")
+
+    for i in range(size):
+        print('#' * size)
