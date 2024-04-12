f735153a2ed0d8fb408ab4cd8e61510742b34720
Esther Ndosi <estherndosi24@gmail.com>
2024-04-12 02:32:18 -0700

    TDD divide matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,11 +1,41 @@
 #!/usr/bin/python3
+"""
+This is the "2-matrix_divided" module
+
+The 2-matrix_divided module supplies one function, matrix_divided().
+For example,
+
+>>> matrix_divided([[2, 4, 6], [8, 10, 12]], 2)
+[[1.0, 2.0, 3.0],[4.0, 5.0, 6.0]]
+"""
+
+
 def matrix_divided(matrix, div):
     """Returns the new martix divided by the number div"""
+    new_matrix = []
+    new_list = []
 
-    return all([[type(i) in [int, float] for i in r] for r in matrix])
+    if matrix is None or type(matrix) != list or len(matrix) == 0:
+        raise TypeError('matrix must be a matrix '
+                        '(list of lists) of integers/floats')
 
-matrix = [
-    [1, 2, 'a'],
-    [4, 5, 6]
-]
-print(matrix_divided(matrix, 3))
+    length = len(matrix[0])
+
+    if type(div) not in [int, float]:
+        raise TypeError('div must be a number')
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+
+    for i in range(len(matrix)):
+        if len(matrix[i]) != length:
+            raise TypeError('Each row of the matrix must have the same size')
+        for j in range(len(matrix[i])):
+            if type(matrix[i][j]) not in [int, float]:
+                raise TypeError('matrix must be a matrix '
+                                '(list of lists) of integers/floats')
+            else:
+                new_list.append(round(matrix[i][j] / div, 2))
+        new_matrix.append(new_list)
+        new_list = []
+
+    return new_matrix
