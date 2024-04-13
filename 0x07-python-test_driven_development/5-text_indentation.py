f0863434ef5be7dcc28246ef662874c2e2ffc2ba
Esther Ndosi <estherndosi24@gmail.com>
2024-04-13 03:15:10 -0700

    TDD indent text

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,8 +7,11 @@ Which prints the `text` and add new line after `.`, `?` and `:` characters.
 
 >>> text_indentation("My name. is? Esther:Ndosi")
 My name
- is
- Esther
+
+is
+
+Esther
+
 Ndosi
 """
 
@@ -16,21 +19,15 @@ Ndosi
 def text_indentation(text):
     """Prints text and new line after ., ? and : characters"""
 
-    if type(text) != str:
+    i = 0
+
+    if not isinstance(text, str):
         raise TypeError('text must be a string')
 
-    for i in text:
-        if i in ".?:":
+    while(i < len(text)):
+        print(text[i], end="")
+        if text[i] in ".?:":
             print("\n")
-
-        print(i, end="")
-
-text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
-Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
-Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
-Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
-rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
-stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
-cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
-beatiorem! Iam ruinas videres""")
+            if (i + 1) != len(text) and text[i + 1] == ' ':
+                i += 1
+        i += 1
