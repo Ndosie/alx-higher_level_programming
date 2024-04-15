d0d3aa55fd80c7678660dd403f3b86cf2612a52c
Esther Ndosi <estherndosi24@gmail.com>
2024-04-15 05:52:05 -0700

    Checking inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is 4-inherits_from module
+
+The 4-inherits_from module has one function ``inherits_from(obj, a_class)``
+which return True if `obj` inherit from `a_class` otherwise False
+"""
+
+
+def inherits_from(obj, a_class):
+    """Returns True or False if obj inherits from a_class"""
+
+    if issubclass(obj, a_class):
+        return True
+    return False
