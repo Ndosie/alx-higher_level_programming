925925dfde3a71a2e93ddba0102dcb2fc21959b9
Esther Ndosi <estherndosi24@gmail.com>
2024-04-15 06:12:24 -0700

    Modifying base class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""Define Geometry class"""
+
+
+class BaseGeometry:
+    """Represents a Geometry and its attributes and methods"""
+
+    def area(self):
+        """Raises an error message"""
+
+        raise Exception("area() is not implemented")
