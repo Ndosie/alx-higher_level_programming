72b5820a69b6704613c6e0cc81de174592fe1323
Esther Ndosi <estherndosi24@gmail.com>
2024-04-15 05:20:42 -0700

    Instance of a class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This is 2-is_same_class module
+
+The 2-is_same_class modul has one function `is_same_class(obj, a_class)`
+which returns true if `obj` is an instance of `a_class` otherwise false.
+"""
+
+
+def is_same_class(obj, a_class):
+    """Returns True or False if obj is an instance of a_class"""
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
