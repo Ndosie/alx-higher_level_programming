dc99064a6f2ff32274cbdbe826618d64c26224be
Esther Ndosi <estherndosi24@gmail.com>
2024-04-16 05:52:54 -0700

    JSON string representation

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-import json
-
 """
 This is 3-to_json_string module
 
 The 3-to_json_string module has one function ``to_json_string(my_obj)``
 which returns the json representation of object `my_obj`.
 """
+import json
 
 
 def to_json_string(my_obj):
