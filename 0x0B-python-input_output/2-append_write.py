a2828dcf97e70489596fb6ec603495480e2a19cd
Esther Ndosi <estherndosi24@gmail.com>
2024-04-16 05:25:33 -0700

    Appends to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is 2-append_write module
+
+The 2-append_write module has one function ``append_write(filename="", text="")``
+which appends `text` to a file `filename` and returns the number of characters
+written.
+"""
+
+
+def append_write(filename="", text=""):
+    """Appends text to a file"""
+
+    with open(filename, mode='a', encoding='utf-8') as f:
+       return f.write(text)
