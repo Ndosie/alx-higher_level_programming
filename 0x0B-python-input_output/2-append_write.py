72ffb53fc11b9a0b490fde60e82f83662b4cc885
Esther Ndosi <estherndosi24@gmail.com>
2024-04-16 05:34:14 -0700

    Appends to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,9 +2,9 @@
 """
 This is 2-append_write module
 
-The 2-append_write module has one function ``append_write(filename="", text="")``
-which appends `text` to a file `filename` and returns the number of characters
-written.
+The 2-append_write module has one function ``append_write
+(filename="", text="")``which appends `text` to a file `filename`
+and returns the number of characters written.
 """
 
 
@@ -12,4 +12,4 @@ def append_write(filename="", text=""):
     """Appends text to the end of a UTF8 text file"""
 
     with open(filename, mode='a', encoding='utf-8') as f:
-       return f.write(text)
+        return f.write(text)
