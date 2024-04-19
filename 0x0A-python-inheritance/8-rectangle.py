4a0ed173ab15c2e20c62372e96e319df6f90cda5
Esther Ndosi <estherndosi24@gmail.com>
2024-04-19 06:45:02 -0700

    Creating rectange class as subclass

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define Rectangle class"""
+
+
+class Rectangle(7-base_geometry.BaseGeometry):
+    """Represents Rectangle class"""
+
+    def __init__(self, width, height):
+        """Initializes the object attributes"""
+
+        self.__width = super().integer_validator(width)
+        self.__height = super().integer_validator(height)
