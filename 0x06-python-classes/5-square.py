fbf1cd20a82d9d2f4ad0e733b617be631d81539e
Esther Ndosi <estherndosi24@gmail.com>
2024-04-25 04:59:01 -0700

    Square class

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Define square class"""
+
+
+class Square:
+    """Represents a square with its attributes"""
+
+    def __init__(self, size=0):
+
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.size == 0:
+            print ""
+
+        for i in range(self.size):
+            print("{}".format("#" * self.size))
