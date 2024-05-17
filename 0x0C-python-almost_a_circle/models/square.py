48a8684204ea0e4e540ac4e8d58938f3a0e79b79
Esther Ndosi <estherndosi24@gmail.com>
2024-05-17 08:38:21 -0700

    Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,9 +9,9 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initializes object attributes"""
-        super().__init__(size, size, x, y)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """Returns rectangle representation as string"""
+        """Returns square representation as string"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
                                                  self.y, self.width)
