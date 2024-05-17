e89f2ac49d1abbcdd47a9a896f440c5128745de1
Esther Ndosi <estherndosi24@gmail.com>
2024-05-17 09:06:19 -0700

    Adding update method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,6 +22,31 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """Updates square object attributes"""
+        if args and len(args) != 0:
+            i = 0
+            for arg in args:
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+                i += 1
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == 'id':
+                    self.id = v
+                elif k == 'size':
+                    self.size = v
+                elif k == 'x':
+                    self.x = v
+                elif k == 'y':
+                    self.y = v
+
     def __str__(self):
         """Returns square representation as string"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
