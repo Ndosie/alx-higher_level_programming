626db682f0122ebe2cf2472e45fcc19ca1a72ebf
Esther Ndosi <estherndosi24@gmail.com>
2024-05-17 10:06:32 -0700

    Adding to JSON method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,6 +17,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @static
     def to_json_string(list_dictionaries):
         """Returns JSON string representation"""
 
