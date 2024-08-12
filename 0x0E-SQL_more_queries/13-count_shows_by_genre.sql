ff4b3dc5706943eda8abc4675ea7cd2c49f15edb
Esther Ndosi <estherndosi24@gmail.com>
2024-08-12 06:06:24 -0700

    Number of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,4 +3,4 @@ SELECT g.name AS genre, COUNT(sg.genre_id) AS number_of_shows
 FROM tv_genres AS g INNER JOIN tv_show_genres AS sg
 ON g.id = sg.genre_id
 GROUP BY genre
-ORDER BY number_of_shows ASC;
+ORDER BY number_of_shows DESC;
