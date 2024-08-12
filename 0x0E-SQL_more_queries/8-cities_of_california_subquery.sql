e07938e47c7c6bbe4cf010918daf8276159feee4
Esther Ndosi <estherndosi24@gmail.com>
2024-08-12 04:40:51 -0700

    Cities of California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Lists the cities of Califonia
-SELECT id, name FROM cities, states
-WHERE state_id = states.id
+SELECT id, name FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
