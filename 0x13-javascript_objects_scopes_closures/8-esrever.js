bd447e65f2ae8470af179b387e76b0eacb089975
Esther Ndosi <estherndosi24@gmail.com>
2024-08-15 06:50:51 -0700

    Esrever

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  let l = list.length - 1;
+  let i = 0;
+  while ((l - 1) > 0) {
+    const t = list[l];
+    list[l] = list[i];
+    list[i] = t;
+    i++;
+    l--;
+  }
+  return list;
+};
