d95fd2ee11d41b2961a376625b20853309d8bec7
Esther Ndosi <estherndosi24@gmail.com>
2024-08-23 03:18:23 -0700

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cus = db.cursor()
-    cus.execute("""SELECT cities.name FROM cities INNER JOIN states ON
-                states.id=cities.state_id WHERE states.name='{}'""".format(sys.argv[4]))
+    cus.execute("""SELECT cities.name FROM cities
+                INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4]))
     recs = cus.fetchall()
     for rec in recs:
         print(rec)
