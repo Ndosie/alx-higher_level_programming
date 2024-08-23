a1a21d6bb376d3c3d08fdd461e4b8245d5f2d1d2
Esther Ndosi <estherndosi24@gmail.com>
2024-08-23 02:58:29 -0700

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     cus = db.cursor()
     cus.execute("SELECT cities.id, cities.name, states.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id"
+                cities INNER JOIN states ON states.id=cities.state_id")
     recs = cus.fetchall()
     for rec in recs:
         print(rec)
