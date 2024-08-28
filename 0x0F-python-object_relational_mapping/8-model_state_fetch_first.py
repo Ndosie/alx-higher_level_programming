e371cd11980b8ebad06baa0ae813512dbee3862f
Esther Ndosi <estherndosi24@gmail.com>
2024-08-28 03:15:23 -0700

    First state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     session = Session()
     state = session.query(State).first()
     if state is None:
-        print(Nothing)
+        print("Nothing")
     else:
         print("{}: {}".format(state.id, state.name))
     session.close()
